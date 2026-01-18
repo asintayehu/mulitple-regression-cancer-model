a5f792bf41b95214be83f845dc907b0e2a5e2368
Aaron <asintayehu@proton.me>
2026-01-18 18:41:59 -0500

    working on model.py

--- src/model.py
+++ src/model.py
@@ -8,17 +8,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 class Model():
     def __init__(self):
-        _file_path = '~/poincare/cancer_regression/data/cancer_reg.csv'
-
-        self.cancer_df = pd.read_csv(_file_path) # maybe revise file names
-
-        self.train_set, self.test_set = train_test_split(self.cancer_df, test_size=0.2)
-
-        # only using values which are relevant to us, drop labels
-        self.cancer_df = self.train_set.drop("TARGET_deathRate", axis=1)[["medIncome", "povertyPercent", "PctPublicCoverage"]].copy()
+        file_path = r"~/poincare/cancer_regression/data/cancer_reg.csv"
+        cancer_df = pd.read_csv(file_path)
+        self.train_set, self.test_set = train_test_split(cancer_df, test_size = 0.2) # now have a training set and a testing set! Yay!
+        self.cancer_df_train_set = self.train_set.drop("TARGET_deathRate", axis=1)[["povertyPercent", "medIncome","PctPublicCoverage"]].copy()
         self.cancer_labels = self.train_set["TARGET_deathRate"].copy()
 
     def predict(self):
@@ -26,14 +23,14 @@ class Model():
         self.lin_reg.fit(self.clean_data(self.cancer_df))
 
     def clean_data(self):
-        self.imputer = SimpleImputer(strategy='mean', missing_values=np.nan)
-        self.cancer_df = self.cancer_df.dropna()
-        self.imputer.fit_transform(self.cancer_df)
+        imputer = SimpleImputer(strategy='mean', missing_values=np.nan)
+        cancer_numerical = self.cancer_df_train_set
+        imputer.fit_transform(cancer_numerical)
         numeric_pipeline = Pipeline([('imputer', SimpleImputer(strategy='median')), ('std_scaler', StandardScaler())])
-        numeric_attribs = list(self.cancer_df)
-        full_pipeline = ColumnTransformer([('num', numeric_pipeline, numeric_attribs)])
-        self.cancer_final = full_pipeline.fit_transform(self.cancer_df)
-        return self.cancer_final
-
-
+        full_pipeline = Pipeline([('num', numeric_pipeline)])
+        self.cancer_prepped = full_pipeline.fit_transform(cancer_numerical)
+        return self.cancer_prepped
+    
+    def evaluate(self):
+        pass
     
